a40e5b792095824ab356328d84028822ac142d51
hebertl <lena.hebert@edu.esiee.fr>
2024-11-28 09:25:05 +0000

    creation de trois fonctions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #### Fonctions secondaires
+"""syracuse 06 """
 
 
 # imports
@@ -6,6 +7,7 @@ from plotly.graph_objects import Scatter, Figure
 
 ### NE PAS MODIFIER ###
 def syr_plot(lsyr):
+    """ afficher graphique"""
     title = "Syracuse" + " (n = " + str(lsyr[0]) + " )"
     fig = Figure({  'layout':   { 'title': {'text': title},
                                 'xaxis': {'title': {'text':"x"}},
@@ -32,8 +34,15 @@ def syracuse_l(n):
         list: la suite de Syracuse de source n
     """
 
-    # votre code ici 
-    l = [ ]
+    # votre code ici
+    l = [n]
+    while n !=1 :
+        if n%2 == 0 :
+            n = n/2
+            l.append(n)
+        else :
+            n = n*3 +1
+            l.append(n)
     return l
 
 def temps_de_vol(l):
@@ -45,11 +54,9 @@ def temps_de_vol(l):
     Returns:
         int: le temps de vol
     """
-    
     # votre code ici
 
-    n = 0
-    return n
+    return len(l)-1
 
 def temps_de_vol_en_altitude(l):
     """Retourne le temps de vol en altitude d'une suite de Syracuse
@@ -62,9 +69,14 @@ def temps_de_vol_en_altitude(l):
     """
 
     # votre code ici
+    tva = 0
+    for i,elt in enumerate(l) :
+        if elt<l[0]:
+            tva = i -1
+            break
+    return tva
 
-    n = 0
-    return n
+
 
 
 def altitude_maximale(l):
@@ -76,21 +88,20 @@ def altitude_maximale(l):
     Returns:
         int: l'altitude maximale
     """
-    
     # votre code ici
-    
-    n = 0
-    return n
+    return max(l)
+
 
 
 #### Fonction principale
 
 
 def main():
-
+    """appels principaux"""
     # vos appels à la fonction secondaire ici
     lsyr = syracuse_l(15)
-    syr_plot(lsyr)
+    print(syracuse_l(15))
+    #syr_plot(lsyr)
     print(temps_de_vol(lsyr))
     print(temps_de_vol_en_altitude(lsyr))
     print(altitude_maximale(lsyr))
